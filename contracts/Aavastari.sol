fe34ae3fd267d16f1b01043c0b2d08f568c6cf8a
Hassan Sarfraz <103070301+Hassanawi@users.noreply.github.com>
2024-07-15 09:09:13 +0500

    .

--- contracts/Aavastari.sol
+++ contracts/Aavastari.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "./AvastariNFT.sol";
 
 contract Avastari is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
     constructor(address initialOwner)
