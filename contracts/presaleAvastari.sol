7f95a099d5300d09d984634b27011cf290429e2e
Hassan Sarfraz <103070301+Hassanawi@users.noreply.github.com>
2024-08-05 11:28:24 +0500

    all  errors resolved

--- contracts/presaleAvastari.sol
+++ contracts/presaleAvastari.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
